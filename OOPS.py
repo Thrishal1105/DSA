0c40df934c16b256027159039a729750187a65ec
Thrishal1105 <dharshanojthrishal11@gmail.com>
2026-07-29 15:06:28 +0530

    commit

---
+++ OOPS.py
@@ -0,0 +1,13 @@
+
+
+""" OOPS (Object-Oriented Programming System) is a programming paradigm that uses objects and classes to structure code.
+    It allows for the creation of reusable and modular code by encapsulating data and behavior into objects. 
+"""
+
+""" Class --> A class is a blueprint for creating objects. 
+    It defines a set of attributes and methods that the created objects will have. 
+    In Python, classes are defined using the `class` keyword.
+
+    Object --> An object is an instance of a class.
+"""
+
